d07db56289921a3e9a8d9fba5319bb2115bbd617
goldarowana <arowana.java@qq.com>
2017-11-16 16:14:27 +0800

    add area page

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -16,6 +16,6 @@ class Search extends React.Component {
 }
 
 
-// 使用 require.ensure 异步加载，还不支持 ES6 的 export 
+// 使用 require.ensure 异步加载 ，还不支持 ES6 的 export
 // module.exports = Search
 export default Search
